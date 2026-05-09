7d0955053f970b5e31bbab43a3db7f4d45dacf74
eran187-cell <eran187@users.noreply.github.com>
2026-05-09 17:33:36 +0300

    Inputs: switch item/note fields to type=search + extra anti-autofill attrs to suppress iOS/Android suggestion bar

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='ss-test-v75';
+const CACHE='ss-test-v76';
 self.addEventListener('install',()=>self.skipWaiting());
 self.addEventListener('activate',e=>e.waitUntil(clients.claim()));
 self.addEventListener('fetch',e=>{
