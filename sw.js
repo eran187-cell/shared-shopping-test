28acf2bb3e00ab72ba195fa0174aee8a5ec8819a
eran187-cell <eran187@users.noreply.github.com>
2026-05-09 23:13:55 +0300

    Version reporter: drawer footer shows live SW cache name; toast 'עודכן בהצלחה' on version change

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='ss-test-v83';
+const CACHE='ss-test-v84';
 self.addEventListener('install',()=>self.skipWaiting());
 self.addEventListener('activate',e=>e.waitUntil(clients.claim()));
 self.addEventListener('fetch',e=>{
