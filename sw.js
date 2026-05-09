d94cc104423b19805bfa77914e8575bdd8536ace
eran187-cell <eran187@users.noreply.github.com>
2026-05-09 12:42:10 +0300

    Filters: narrower tabs, count pinned to left edge, pop animation on change

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='ss-test-v54';
+const CACHE='ss-test-v55';
 self.addEventListener('install',()=>self.skipWaiting());
 self.addEventListener('activate',e=>e.waitUntil(clients.claim()));
 self.addEventListener('fetch',e=>{
