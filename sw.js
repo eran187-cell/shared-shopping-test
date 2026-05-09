184d5247c084fc0d758265023773977e5e47836b
eran187-cell <eran187@users.noreply.github.com>
2026-05-09 23:43:38 +0300

    Announcements: paint App Badging API dot on PWA home-screen icon when message pending

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='ss-test-v88';
+const CACHE='ss-test-v89';
 self.addEventListener('install',()=>self.skipWaiting());
 self.addEventListener('activate',e=>e.waitUntil(clients.claim()));
 self.addEventListener('fetch',e=>{
