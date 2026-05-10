8eab2b107a1f16c457366ed74e55a877a4365ce5
eran187-cell <eran187@users.noreply.github.com>
2026-05-10 12:54:43 +0300

    Support: Android uses Gmail web compose URL (skips intent picker); iOS/desktop keep mailto:

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='ss-test-v109';
+const CACHE='ss-test-v110';
 self.addEventListener('install',()=>self.skipWaiting());
 self.addEventListener('activate',e=>e.waitUntil(clients.claim()));
 self.addEventListener('fetch',e=>{
