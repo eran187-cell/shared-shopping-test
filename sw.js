df179490eaa13b815b42eacf2ed6208a2b17eaad
eran187-cell <eran187@users.noreply.github.com>
2026-05-10 10:24:49 +0300

    Inputs: unify visual style of all note/name fields with main item input (12px radius, 1.5px border, 16px font, focus state)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='ss-test-v99';
+const CACHE='ss-test-v100';
 self.addEventListener('install',()=>self.skipWaiting());
 self.addEventListener('activate',e=>e.waitUntil(clients.claim()));
 self.addEventListener('fetch',e=>{
