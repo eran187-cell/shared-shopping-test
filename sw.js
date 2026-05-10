92c1b99b5957b7c19ef3e516abe26d7fde34823b
eran187-cell <eran187@users.noreply.github.com>
2026-05-10 09:08:07 +0300

    Announcements: targeting via filter / checkbox list / single code; live preview card

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='ss-test-v92';
+const CACHE='ss-test-v93';
 self.addEventListener('install',()=>self.skipWaiting());
 self.addEventListener('activate',e=>e.waitUntil(clients.claim()));
 self.addEventListener('fetch',e=>{
