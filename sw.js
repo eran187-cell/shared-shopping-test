a736004399919b5d56addbb6488eb4e5075f126c
eran187-cell <eran187@users.noreply.github.com>
2026-05-10 15:20:29 +0300

    Version distribution: clickable donut pie chart with device drill-down + 'send message to these users' shortcut

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='ss-test-v118';
+const CACHE='ss-test-v119';
 self.addEventListener('install',()=>self.skipWaiting());
 self.addEventListener('activate',e=>e.waitUntil(clients.claim()));
 self.addEventListener('fetch',e=>{
