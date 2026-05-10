6c9d4321df9940590d520e5ba04cc30929e656f7
eran187-cell <eran187@users.noreply.github.com>
2026-05-10 12:09:38 +0300

    AI sort toggle: 3s flourish on activate, quick 0.5s spin on deactivate

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='ss-test-v105';
+const CACHE='ss-test-v106';
 self.addEventListener('install',()=>self.skipWaiting());
 self.addEventListener('activate',e=>e.waitUntil(clients.claim()));
 self.addEventListener('fetch',e=>{
