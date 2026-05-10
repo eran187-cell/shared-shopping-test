0b08f9f6655d2938fef58571b39c83dc3c15ad05
eran187-cell <eran187@users.noreply.github.com>
2026-05-10 11:44:34 +0300

    Light mode: darken --mut from #7aa090 to #5d8a7a (WCAG 4.5:1 contrast on white)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='ss-test-v101';
+const CACHE='ss-test-v102';
 self.addEventListener('install',()=>self.skipWaiting());
 self.addEventListener('activate',e=>e.waitUntil(clients.claim()));
 self.addEventListener('fetch',e=>{
