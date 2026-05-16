f14b7e2f4a49806410cba0bc3ca1ba1d58100bbb
eran187-cell <eran187@users.noreply.github.com>
2026-05-16 14:29:35 +0300

    Viewport: lock zoom (maximum-scale=1, user-scalable=no) for native-app feel

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='ss-test-v132';
+const CACHE='ss-test-v133';
 self.addEventListener('install',()=>self.skipWaiting());
 self.addEventListener('activate',e=>e.waitUntil(clients.claim()));
 self.addEventListener('fetch',e=>{
